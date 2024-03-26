06163a791ac916d37f39161b57dea75c8e90dd51
Radostin Tasev <125496636+RadostinTasev19@users.noreply.github.com>
2024-03-26 17:23:54 +0200

    Update themes-list.component.ts

--- 06.WorkshopModulesAndRouting/workshop-modules/src/app/theme/themes-list/themes-list.component.ts
+++ 06.WorkshopModulesAndRouting/workshop-modules/src/app/theme/themes-list/themes-list.component.ts
@@ -9,23 +9,33 @@ import { UserService } from 'src/app/user/user.service';
   styleUrls: ['./themes-list.component.css'],
 })
 export class ThemesListComponent implements OnInit {
-  themes: Theme[] | null = [];
+  /*
+  the class implements the ngOnInit lifecycle hook
+  ngOninit() must be defined in any class that implements this interface
+  
+  */
+  themes: Theme[] | null = [];// we set themes property to type Theme[] or null and set its value to an empty array
   isLoading: boolean = true;
 
   constructor(private api: ApiService, private userService: UserService) {}
 
   get isLoggedIn(): boolean {
     return this.userService.isLogged;
+    //this method will return boolean value from service userService
   }
 
   get userId(): string {
     return this.userService.user?.id || '';
+  // this method will return the userService id from service userService
   }
 
   ngOnInit(): void {
     this.api.getThemes().subscribe((themes) => {
       this.themes = themes;
-
+  /*
+  this is a lifecycle hook which gets the returned value from getThemes method and subscribes to it.
+  then it assigns the returned observable to themes property
+  */
       setTimeout(() => {
         this.isLoading = false;
       }, 1000);
@@ -33,8 +43,16 @@ export class ThemesListComponent implements OnInit {
   }
 
   isSubscribed(theme: Theme) {
+    /*
+    theme.subscribers - an array of user IDs who are subscribed to the theme.
+    find method is used to search this array for a user ID that matches this.userService.user?.id
+    
+    */
     const isSubscribedUser = theme.subscribers.find((s) => {
       return s === this.userService.user?.id;
+      /*
+      this function is used to check if the current user is subscribed to a specific theme
+      */
     });
 
     return !!isSubscribedUser;
