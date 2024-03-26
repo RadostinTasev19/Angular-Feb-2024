429e6d904a59683692c243ccf3aeece5d515c7d8
Radostin Tasev <125496636+RadostinTasev19@users.noreply.github.com>
2024-03-26 17:02:25 +0200

    Update current-theme.component.ts

--- 06.WorkshopModulesAndRouting/workshop-modules/src/app/theme/current-theme/current-theme.component.ts
+++ 06.WorkshopModulesAndRouting/workshop-modules/src/app/theme/current-theme/current-theme.component.ts
@@ -10,16 +10,25 @@ import { Theme } from 'src/app/types/theme';
 })
 export class CurrentThemeComponent implements OnInit {
   theme = {} as Theme;
-
+// declares a theme property with an empty object that is cast to Theme type
   constructor(
     private apiService: ApiService,
     private activeRoute: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void { // this is a lifecycle hook method and is called once when the component is initialized
+    /*
+    -this line subscribes to the params Observable from Activated Route
+    - it will execute the provided function whenever the route parameters change.
+    - the function receives the route parameters as its argument.
+    */
     this.activeRoute.params.subscribe((data) => {
       const id = data['themeId'];
-
+  /*
+  this line calls the getTheme method from ApiService with the themeId as an argument
+  It then subscribes to the returned Observable.
+  this.theme = theme: assigns the response from the getTheme method to the theme property of the component.
+  */
       this.apiService.getTheme(id).subscribe((theme) => {
         this.theme = theme;
       });
